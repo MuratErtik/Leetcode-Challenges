6a5c8f52c26d72e50171d96886e6ae95fbe38ac8
Murat Ertik <dmuratertik1@gmail.com>
2025-09-16 12:27:10 -0700

    power of two

---
+++ src/main/java/org/example/PowerOfTwo.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+ */
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else{
+            while(n%2==0){
+                if(n==2){
+                    return true;
+                }
+                n=n/2;
+            }
+            return false;
+
+        }
+    }
+}
